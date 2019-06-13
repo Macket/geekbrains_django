4972389dda0cecf4ef1703de23667fc5ba1831b6
Makeev Ivan <makeev@phystech.edu>
2019-06-13 22:36:38 +0300

    Урок 4. Аутентификация и регистрация пользователя

--- authapp/models.py
+++ authapp/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# class ShopUser(AbstractUser):
-#     class Meta:
-#         verbose_name = 'пользователь'
-#         verbose_name_plural = 'пользователи'
-#
-#     avatar = models.ImageField(upload_to='users_avatars', blank=True)
-#     age = models.PositiveIntegerField(verbose_name='возраст')
-#
-#     def __str__(self):
-#         return self.username
+class ShopUser(AbstractUser):
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    avatar = models.ImageField(upload_to='users_avatars', blank=True)
+    age = models.PositiveIntegerField(verbose_name='возраст')
+
+    def __str__(self):
+        return self.username
