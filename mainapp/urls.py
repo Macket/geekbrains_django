6d6af8209b44f8a21596e768e1de258dbf7c1906
Makeev Ivan <makeev@phystech.edu>
2019-06-17 19:20:20 +0300

    Подготовка к Уроку 5

---
+++ mainapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from mainapp.views import products
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', products, name='index'),
+    path('category/<int:pk>/', products, name='category'),
+]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,28 +1,16 @@
 from django.shortcuts import render
-import json, os
 from .models import Product
-
-JSON_PATH = 'mainapp/json'
-
-
-def loadMenuFromJSON():
-    with open(os.path.join(JSON_PATH, 'menu.json'), 'r') as infile:
-        return json.load(infile)
 
 
 def main(request):
-    links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu, 'username': 'alexey'}
+    context = {'username': 'alexey'}
     return render(request, 'mainapp/main.html', context)
 
 
 def products(request):
-    links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu, 'products': Product.objects.all()}
+    context = {'products': Product.objects.all()}
     return render(request, 'mainapp/products.html', context)
 
 
 def contacts(request):
-    links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu}
-    return render(request, 'mainapp/contacts.html', context)
+    return render(request, 'mainapp/contacts.html')
