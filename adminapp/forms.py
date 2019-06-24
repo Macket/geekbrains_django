da31b40b914e3ab6cea4d4a6cf2d10633f9f70eb
Makeev Ivan <makeev@phystech.edu>
2019-06-24 21:51:13 +0300

    Урок 7. Собственная админка

---
+++ adminapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from mainapp.models import Product
+
+
+class ProductAdminForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
