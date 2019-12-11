f293772239bdf897c5f70c9343af54eb05fb8ba8
ilmix007 <imi87@yandex,ru>
2019-12-12 01:25:22 +0500

    ДЗ 3 сделал все кроме вывода изображений, проблема с cсылками url

---
+++ evoshop/evoshop/urls.py
@@ -0,0 +1,31 @@
+"""evoshop URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import mainapp.views as mainapp
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', mainapp.main, name = 'main'),
+    path('contacts/', mainapp.contacts, name = 'contacts'),
+    path('catalog/', mainapp.catalog, name = 'catalog'),
+    path('catalog/product/', mainapp.product, name = 'product'),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
